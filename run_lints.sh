bd67ab592a6c5346938a84b8bd85ad984cea4a9e
Maciej Pacula <maciej.pacula@gmail.com>
2018-12-04 14:21:48 -0500

    Add example output

--- run_lints.sh
+++ run_lints.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "Running pylint..."
-pylint --output-format=colorized pycrumbs/ tests/
+pylint --output-format=colorized pycrumbs/ tests/ examples/
 
 echo -e "\n\nRunning flake8..."
-flake8 --max-line-length=120 pycrumbs/ tests/
+flake8 --max-line-length=120 pycrumbs/ tests/ examples/
 
 echo -e "\n\nAll lints passed successfully."
